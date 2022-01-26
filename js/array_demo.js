c023a92f3e35f62d1041ac670986cb4f1da1f54d
srikanthpragada <srikanthpragada@gmail.com>
2022-01-26 20:58:04 +0530

    26-JAN

--- js/array_demo.js
+++ js/array_demo.js
@@ -1,11 +1,38 @@
-let names = ["Java", "TypeScript", "Python"]
-console.log(names)
-console.log(names.length)
 
-// Print each value
-for (var n of names)
-    console.log(n)
 
-names["abc"] = "Xyz"
-for (var n in names)
-    console.log(n)
+// let names = ["Java", "TypeScript", "Python"]
+// console.log(names)
+// console.log(names.length)
+
+// // Print each value
+// for (var n of names)
+//     console.log(n)
+
+// names["abc"] = "Xyz"
+
+// for (var n in names)
+//     console.log(n)
+
+var marks = [90,87,45,34,80]
+
+function pass(n)
+{
+    return n > 50;
+}
+
+console.log(marks.filter(pass))
+
+// Anonymous function 
+var selected = marks.filter(function(v)
+        {  
+         return v > 50;
+        }
+);        
+console.log(selected)
+
+// Arrow function 
+var selected = marks.filter(v => v > 50); 
+console.log(selected)
+
+// print all values in the array 
+marks.forEach( v => console.log(v))
